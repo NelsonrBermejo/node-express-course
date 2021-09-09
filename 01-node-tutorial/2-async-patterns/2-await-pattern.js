386a8635cfd75a1d5d0ff8742d6ffaf11e75a803
NelsonrBermejo <bermejonelson.r@gmail.com>
2021-09-08 22:49:58 -0300

    Await read and write file asinc

--- 01-node-tutorial/2-async-patterns/2-await-pattern.js
+++ 01-node-tutorial/2-async-patterns/2-await-pattern.js
@@ -1,36 +1,53 @@
-const { readFile, writeFile } = require('fs').promises
-// const util = require('util')
-// const readFilePromise = util.promisify(readFile)
-// const writeFilePromise = util.promisify(writeFile)
+const { readFile,writeFile } = require ('fs')
+const util = require('util');
 
-const start = async () => {
-  try {
-    const first = await readFile('./content/first.txt', 'utf8')
-    const second = await readFile('./content/second.txt', 'utf8')
-    await writeFile(
-      './content/result-mind-grenade.txt',
-      `THIS IS AWESOME : ${first} ${second}`,
-      { flag: 'a' }
-    )
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
+
+const start = async() =>{
+    try{
+    //await...promise from result
+    // const first = await getText('./content/first.txt')
+    const first = await readFilePromise('./content/first.txt', 'utf8')
+    //const second = await getText('./content/second.txt')
+    const second = await readFilePromise('./content/second.txt', 'utf8')
+    await writeFilePromise(
+        './content/result-mind-grenade.txt',`THIS IS AWESOME : ${first} ${second}`,
+        { flag: 'a' }
+        )
     console.log(first, second)
-  } catch (error) {
-    console.log(error)
-  }
-}
 
+    }
+    catch (error){
+     console.log(error)
+    }
+    
+}
+//invoca
 start()
 
-// const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     readFile(path, 'utf8', (err, data) => {
-//       if (err) {
-//         reject(err)
-//       } else {
-//         resolve(data)
-//       }
-//     })
-//   })
-// }
-// getText('./content/first.txt')
-//   .then((result) => console.log(result))
-//   .catch((err) => console.log(err))
+
+//obtengo el texto de un sendero
+//const getText = (path) =>{
+//    return new Promise((resolve,reject)=>{
+        //encapsulo
+        //readFile('./content/first.txt', 'utf8', (err, data) => {
+//        readFile(path,'utf8', (err, data) => {
+//            if(err) {
+            // return
+ //           reject(err)
+ //           }
+   //         else {
+                //console.log(data)
+      //          resolve(data)
+     //       }
+       // })
+//encapsulado
+//    })
+//}
+
+
+
+//getText('./content/first.txt')
+//.then(result => console.log(result))
+//.catch(err=> console.log(err))
